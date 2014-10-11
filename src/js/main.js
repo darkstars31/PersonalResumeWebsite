85613d1c9c7a78d113043704c771b75672fd6839
ASanti <Darkstars31@gmail.com>
2014-10-10 23:57:29 -0500

    EJS Templating
    EJS Templating is now setup on the project

--- src/js/main.js
+++ src/js/main.js
@@ -5,13 +5,14 @@
 // Paths for JS Loading
 require.config({
 paths: {
-        'jquery': 'lib/jquery',
-        'jqueryui': 'lib/jquery-ui',
-        'underscore': 'lib/underscore'
+        'jquery': 'lib/jquery/jquery',
+        'jqueryui': 'lib/jquery/jquery-ui',
+        'underscore': 'lib/underscore/underscore',
+        'ejs': 'lib/ejs/ejs'
     },
 });
 
-requirejs(['jquery'], function( $ ) {
+requirejs(['jquery','ejs'], function( $ ) {
     //jQuery, loaded and can be used here now.
     
     $('.p-skill1, .p-skill2, .p-skill3').hide();
@@ -22,5 +23,14 @@ requirejs(['jquery'], function( $ ) {
             $( '.p-skill2' ).slideDown();}, function () { $('.p-skill2').slideUp()});
     $('.skill-img3').hover( function () {
             $( '.p-skill3' ).slideDown();}, function () { $('.p-skill3').slideUp()});
+
+    var data = 'Dynamic content';
+    
+    var html = new EJS({url: '../stuff.ejs'})
+                        .render({
+                            data: data
+                        });
+    
+    $('.ejs-template').append(html);
     
 });
